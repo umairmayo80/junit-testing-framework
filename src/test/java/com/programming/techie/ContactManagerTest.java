27335d0ecdc38caa6523fee39cf0da054d65c47b
umairmayo80 <aliumer1080@gmail.com>
2023-08-22 18:06:54 +0500

    Junit Test Lifecycle

--- src/test/java/com/programming/techie/ContactManagerTest.java
+++ src/test/java/com/programming/techie/ContactManagerTest.java
@@ -1,12 +1,26 @@
 package com.programming.techie;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// by default junit create separate instance of class for each test case function
+// .per_class changes to singleton
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ContactManagerTest {
+
+    ContactManager contactManager;
+
+    @BeforeAll
+    public void setupAll(){
+        System.out.println("It will run at the very first of the lifecycle.");
+    }
+
+    @BeforeEach
+    public void setup(){
+        //create a fresh contactManager for each test case
+        contactManager = new ContactManager();
+    }
 
     @Test
     public void shouldCreateContact() {
@@ -28,6 +42,18 @@ class ContactManagerTest {
         Assertions.assertThrows(RuntimeException.class, ()->{
             contactManager.addContact("", "Doe", "0123456789");
         });
+    }
+
+
+    @AfterEach
+    public void tearDown(){
+        System.out.println("It will be executed after each test case");
+    }
+
+    @AfterAll
+    public void tearDownAll(){
+        contactManager = null; // clean up
+        System.out.println("It will be executed at the end of test lifecycle, ususally for clean upp");
     }
 
 }
